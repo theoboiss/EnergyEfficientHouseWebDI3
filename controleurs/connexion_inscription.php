b51916f97facd693fe3b379e4c8b716f00767a54
theo-boi <48034060+theo-boi@users.noreply.github.com>
2020-12-28 18:13:37 +0100

    Added header and footer to connexion/inscription

--- controleurs/connexion_inscription.php
+++ controleurs/connexion_inscription.php
@@ -10,6 +10,7 @@ include('./modele/requetes.connexion_inscription.php');
 if (!isset($_POST['fonction']) || empty($_POST['fonction'])) {
     $function = "";
     $vue = "connexion_inscription";
+    $title = "Connexion/Inscription";
     $alerte = "";
 } else if (!isset($_POST['nomUser']) || empty($_POST['nomUser'])
         || !isset($_POST['emailUtilisateur']) || empty($_POST['emailUtilisateur'])
@@ -18,6 +19,7 @@ if (!isset($_POST['fonction']) || empty($_POST['fonction'])) {
         || !isset($_POST['ageUtilisateur']) || empty($_POST['ageUtilisateur']) ) {
     $function = "";
     $vue = "connexion_inscription";
+    $title = "Connexion/Inscription";
     $alerte = "Formulaire incomplet";
 } else {
     $function = $_POST['fonction'];
@@ -32,6 +34,7 @@ if (!isset($_POST['fonction']) || empty($_POST['fonction'])) {
                 case 'connexion':
                     //Verifie le mdp
                     $vue = "connexion_inscription";
+                    $title = "Connexion/Inscription";
 
                     $result = connexionUtilisateur($bdd, $nomUser, $mdpUtilisateur);
                     if ($result) {
@@ -48,6 +51,7 @@ if (!isset($_POST['fonction']) || empty($_POST['fonction'])) {
                 case 'inscription':
                     //Alerte que le compte existe deja
                     $vue = "connexion_inscription";
+                    $title = "Connexion/Inscription";
 
                     $alerte = "Ce nom d'utilisateur est deja pris";
                     break;
@@ -59,6 +63,7 @@ if (!isset($_POST['fonction']) || empty($_POST['fonction'])) {
                 case 'connexion':
                     //Alerte que le compte n'existe pas
                     $vue = "connexion_inscription";
+                    $title = "Connexion/Inscription";
 
                     $result = connexionUtilisateur($bdd, $nomUser, $mdpUtilisateur);
                     if ($result) {
@@ -76,6 +81,7 @@ if (!isset($_POST['fonction']) || empty($_POST['fonction'])) {
                 case 'inscription':
                     //Insere le compte dans la BDD
                     $vue = "connexion_inscription";
+                    $title = "Connexion/Inscription";
 
                     if ($mdpUtilisateur == $_POST['mdpUtilisateurVerification']) {
                         $emailUtilisateur = $_POST['emailUtilisateur'];
@@ -103,6 +109,6 @@ if (!isset($_POST['fonction']) || empty($_POST['fonction'])) {
     }
 }
 
-//include ('vues/header.php');
+include ('vues/header.php');
 include ('vues/' . $vue . '.php');
-//include ('vues/footer.php');
+include ('vues/footer.php');
