f1641f63a638f9da21658c0dd3982096fcb9aea2
theo-boi <48034060+theo-boi@users.noreply.github.com>
2021-01-04 10:17:59 +0100

    Updated : acceuil.php

--- vues/accueil.php
+++ vues/accueil.php
@@ -12,10 +12,12 @@
         </head>
 
     	<body>
+			<?php if (estConnecte()) echo '
 			<p><a href="index.php?cible=principal&fonction=parametrage">Gestion des entités paramétrables</a></p>
 			<p><a href="index.php?cible=appareils&fonction=afficherAppareils">Mes Appareils</a></p>
 			<p><a href="index.php?cible=appartements&fonction=afficherAppartements">Mes Appartements</a></p>
 			<p><a href="index.php?cible=maisons&fonction=afficherMaisons">Mes Maisons</a></p>
+			'?>
 			<p><a href="index.php?cible=connexion_inscription">Connexion/Deconnexion</a></p>
 
 			<?php if (isset($alerte)) echo AfficheAlerte($alerte); ?>
--- vues/fonctions.php
+++ vues/fonctions.php
@@ -9,8 +9,12 @@
  * @param string|null $alerte
  */
 function AfficheAlerte(?string $alerte) {
-    
     if(!is_null($alerte) && !empty($alerte)) {
         return "<p><strong><i>{$alerte}</i></strong></p>";
     }
+}
+
+function estConnecte(): int {
+    if (isset($_SESSION['etat'])) return ($_SESSION['etat'] == 'actif');
+    else return 0;
 }
