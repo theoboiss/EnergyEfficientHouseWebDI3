96a3fea1f2da1a4e1ac6514ecf2560e9a34790cc
G-Delp <75621972+G-Delp@users.noreply.github.com>
2021-01-06 11:14:54 +0100

    Update ressources.php

--- controleurs/ressources.php
+++ controleurs/ressources.php
@@ -28,3 +28,21 @@ switch ($function) {
         }
         
         break;
+        
+        
+        
+    default:
+        // si aucune fonction ne correspond au paramètre function passé en GET
+        $vue = "erreur404";
+        $title = "error404";
+        $message = "Erreur 404 : la page recherchée n'existe pas.";
+        
+        
+        
+        
+        
+        
+        
+include ('vues/header.php');
+include ('vues/' . $vue . '.php');
+include ('vues/footer.php');
