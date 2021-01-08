d1671b2bad0ea90ee7b78065faad89389fdfda70
G-Delp <75621972+G-Delp@users.noreply.github.com>
2021-01-08 15:47:50 +0100

    Update detail_appareil.php

--- vues/detail_appareil.php
+++ vues/detail_appareil.php
@@ -24,11 +24,14 @@
                 </tr>
             </thead>
             <tbody>
+                <?php foreach($affRessources as $element) { ?>
                 <tr>
-                    <td class="object_td"><?php echo $affRessources[3]; ?></td>
-                    <td class="object_td"><?php echo $affRessources[4]; ?></td>
-                    <td class="object_td"><?php echo $affRessources[5]; ?></td>
-                    <td class="object_td"><?php echo $affRessources[6]; ?></td>
+                    <td class="object_td"><?php echo $element[3]; ?></td>
+                    <td class="object_td"><?php echo $element[4]; ?></td>
+                    <td class="object_td"><?php echo $element[5]; ?></td>
+                    <td class="object_td"><?php echo $element[6]; ?></td>
+                </tr>
+                <?php } ?>
             </tbody>
             <?php echo AfficheAlerte($alerte); ?>
     </body>
