ac242de75e0677d66d92ad215a5e406e7017023e
G-Delp <75621972+G-Delp@users.noreply.github.com>
2021-01-08 15:03:58 +0100

    Update detail_appareil.php

--- vues/detail_appareil.php
+++ vues/detail_appareil.php
@@ -6,21 +6,18 @@
 
 <!DOCTYPE html>
     <html lang="fr">
+        
         <head>
             <meta charset="UTF-8" />
             <link rel="stylesheet" href="public/css/styles_con.css"/>
         </head>
 
     <body>
-
         <p><?php echo $entete; ?></p>
-
-
         <table class="object_table">
             <thead>
         		<tr>
-
-        			<th class="object_td">Libellé</th>
+                    <th class="object_td">Libellé</th>
                     <th class="object_td">Description</th>
                     <th class="object_td">Valeur Critique</th>
                     <th class="object_td">Valeur Idéale</th>
@@ -37,4 +34,5 @@
             </tbody>
             <?php echo AfficheAlerte($alerte); ?>
     </body>
+        
 </html>
