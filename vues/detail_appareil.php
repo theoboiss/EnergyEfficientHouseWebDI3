0fee6692e891d8acbcfe057ec92646980b4df382
G-Delp <75621972+G-Delp@users.noreply.github.com>
2021-01-08 14:20:06 +0100

    Create detail_appareil.php

---
+++ vues/detail_appareil.php
@@ -0,0 +1,23 @@
+<?php 
+/**
+* Vue : liste des ressources
+*/
+?>
+
+<!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <meta charset="UTF-8" />
+            <link rel="stylesheet" href="public/css/styles_con.css"/>
+        </head>
+
+    <body>
+
+        <p><?php echo $entete; ?></p>
+
+
+        <table class="object_table">
+        <thead>
+        		<tr>
+
+        			<th class="object_td">Libellé</th>
