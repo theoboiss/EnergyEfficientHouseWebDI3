eda47b2b95709520cf33038655c1a8fba6554dce
G-Delp <75621972+G-Delp@users.noreply.github.com>
2021-01-08 15:03:19 +0100

    Update detail_appareil.php

--- vues/detail_appareil.php
+++ vues/detail_appareil.php
@@ -17,7 +17,24 @@
 
 
         <table class="object_table">
-        <thead>
+            <thead>
         		<tr>
 
         			<th class="object_td">Libellé</th>
+                    <th class="object_td">Description</th>
+                    <th class="object_td">Valeur Critique</th>
+                    <th class="object_td">Valeur Idéale</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($afficherRessources as $element) { ?>
+                <tr>
+                    <td class="object_td"><?php echo $element['libelle']; ?></td>
+                    <td class="object_td"><?php echo $element['description']; ?></td>
+                    <td class="object_td"><?php echo $element['valCrit']; ?></td>
+                    <td class="object_td"><?php echo $element['valIdeale']; ?></td>
+                <?php } ?>
+            </tbody>
+            <?php echo AfficheAlerte($alerte); ?>
+    </body>
+</html>
