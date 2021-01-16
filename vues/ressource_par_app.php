e09f04c43f8fc32cf5b1a4310412a98a9f362c19
G-Delp <75621972+G-Delp@users.noreply.github.com>
2021-01-16 17:19:04 +0100

    Create ressource_par_app.php

---
+++ vues/ressource_par_app.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+* Vue : ressources par appareil
+*/
+?>
+
+<!DOCTYPE html>
+    <html lang="fr">
+        
+        <head>
+            <meta charset="UTF-8" />
+            <link rel="stylesheet" href="public/css/styles_con.css"/>
+        </head>
