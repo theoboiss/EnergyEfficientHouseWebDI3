cfc6eb925f3220fb8d7b031b87da5f5349a9b05d
G-Delp <75621972+G-Delp@users.noreply.github.com>
2021-01-17 16:30:18 +0100

    Update requetes.afficher_ajouter_ressources.php

--- modele/requetes.afficher_ajouter_ressources.php
+++ modele/requetes.afficher_ajouter_ressources.php
@@ -14,4 +14,13 @@ include("modele/connexion.php");
     $query = 'SELECT * FROM matiere';
     return mysqli_query($bdd, $query);
  }
+
+ function afficherStat(mysqli $bdd){
+  
+    $query = 'SELECT * FROM consoappareil WHERE Id_Appareil IN (SELECT * FROM appareil NATURAL JOIN piece NATURAL JOIN appartement NATURAL JOIN type_appareil
+                                                                WHERE Id_Piece IN (SELECT Id_Piece FROM piece NATURAL JOIN appartement
+                                                                                   WHERE Id_Appartement IN (SELECT Id_Appartement FROM appartement NATURAL JOIN locataire
+                                                                                                            WHERE Id_Utilisateur =' . $_SESSION['id'] . ' AND ((dateFinLocation IS NULL) OR (DATE(NOW()) BETWEEN dateDebutLocation AND dateFinLocation)))))';
+    return mysqli_query($bdd, $query);
+ }
 ?>
